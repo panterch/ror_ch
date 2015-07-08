a96edfd029ebfb0916c5adfb518bcd86faa149f0
Dominique Schuwey <domi@domidesign.ch>
2015-07-08 17:09:56 +0200

    fix typo in jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
-class JobsController < ApplicationController before_action :set_job, only: [:show, :edit, :update, :destroy]
+class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   # GET /jobs
   def index
